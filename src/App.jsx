566d78edf0ce73c634f6323339d0f5fd06a28f93
vicenteaugusto9 <vicenteaugusto180374@gmail.com>
2025-04-20 01:28:09 -0300

    comeco do projeto- e-commerce treino

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='Header-Container'>
+         <div className="top-header">
+          <p>sumer um dois caera acularksajndakslndalknlk</p>
+         </div>
+         <div className="nav-header">
+          <div className="logo-header">
+            <p className='p-logo-header'>Exclusive</p>
+          </div>
+          <div className="navbar">
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Contact</li>
+              <li>About</li>
+              <li>Sing Up</li>
+            </ul>
+          </nav>
+          </div>
+          <div className="input-header">
+            <input type="search" name="search" id="search" placeholder='What are you looking for?' />
+            <div className="icons-svg-carrinho">
+
+            <img src="src\assets\svgs\coracao.svg" alt="" />
+            <img src="src\assets\svgs\carrinho.svg" alt="" />
+          </div>
+          </div>
+          
+          
+         </div>
+      </div>
+    </>
+  )
+}
+
+export default App
